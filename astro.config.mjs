c378317cc4dbcc125aee02a620f0c70525a7840c
lexCoder2 <hi@iarodriguez>
2026-05-21 19:48:01 -0600

    fix: ci domain name prefix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://iarodriguez.com',
-  base: '/me',
   integrations: [sitemap()],
   output: 'static',
 });
